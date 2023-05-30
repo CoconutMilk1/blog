8f47f16b800722dd9aeb9b235587f317425f3d04
椰奶 <11228396+coconut9264@user.noreply.gitee.com>
2023-05-30 13:41:34 +0800

    Revert "第一次项目提交"
    This reverts commit 6a4a1f59

--- src/main/java/com/study/blog/service/CommentService.java
+++
@@ -1,8 +0,0 @@
-package com.study.blog.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.study.blog.entity.Article;
-import com.study.blog.entity.Comment;
-
-public interface CommentService extends IService<Comment> {
-}
--- src/main/java/com/study/blog/service/SignService.java
+++
@@ -1,7 +0,0 @@
-package com.study.blog.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.study.blog.entity.Sign;
-
-public interface SignService extends IService<Sign> {
-}
